8b76a06c94bda2cd86b9e824f6f97a7da08bf2d2
ThunfischEis <ian.risch@gmail.com>
2024-01-30 20:25:04 +0100

    This will cause a merge conflict

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test");
+        System.out.println("This will cause a merge Confict LOL");
     }
 }
