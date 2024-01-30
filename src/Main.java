512ed47b2e9692efcb8a53ea01fad1064f9e6d0e
Bananiklo <elija.gattringer@gmail.com>
2024-01-30 21:25:14 +0100

    variablen

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,28 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("^Test123123123");
-        System.out.println("Das ist ein neuer Test");
-        System.out.println("This will cause a merge Confict LOL");
+        double age = 15.3;
+        String Name = "Bananiklo ";
+
+        System.out.println("Hallo ich bin " + Name + "und bin " + age + " Jahre alt.");
+
+        age = 17.2333;
+
+        System.out.println("Hallo ich bin " + Name + "und bin " + age + " Jahre alt.");
+
+        age += 3;
+        System.out.println(age);
+
+        boolean isOverEighteen = age >= 18 && Name.equals("Bananiklo ");
+        System.out.println("ist über achtzehn: "+isOverEighteen);
+
+        if (isOverEighteen){
+            System.out.println("test");
+        }
+
+
+
     }
 }
+
+
