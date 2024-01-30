524f4c5bf85363ce4e4503864713e8e347a92248
cuby0401 <sebastian.erben@icloud.com>
2024-01-30 20:40:37 +0100

    Merge Conflict Test

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Das ist ein neuer Test");
         System.out.println("This will cause a merge Confict LOL");
     }
 }
