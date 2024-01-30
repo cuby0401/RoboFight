7d4b845993fabf236c422e5d867d4b80386509c3
cuby0401 <sebastian.erben@icloud.com>
2024-01-30 21:25:23 +0100

    var Lesson

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,21 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("^Test123123123");
-        System.out.println("Das ist ein neuer Test");
-        System.out.println("This will cause a merge Confict LOL");
+        double age = 15.3;
+        String name = "Sebi ";
+
+        System.out.println("Hallo ich bin " + name + "und bin " + age + " Jahre alt.");
+        age = 13;
+        System.out.println("Hallo ich bin " + name + "und bin " + age + " Jahre alt.");
+        age += 7;
+        System.out.println("Hallo ich bin " + name + "und bin " + age + " Jahre alt.");
+
+        boolean isOverEighteen = age >= 18 && name.equals("Sebi ");
+
+        System.out.println("Ist über 18: " + isOverEighteen);
+
+        if(isOverEighteen) {
+            System.out.println("HAHHAHA Test");
+        }
     }
 }
